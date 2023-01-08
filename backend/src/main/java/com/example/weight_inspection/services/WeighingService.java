885d2897e92e1cd3f4c8599ecfbfce848e02d2d9
letenay7 <99814299+letenay7@users.noreply.github.com>
2023-01-08 19:16:03 +0100

    Backend weighing logic

---
+++ backend/src/main/java/com/example/weight_inspection/services/WeighingService.java
@@ -0,0 +1,25 @@
+package com.example.weight_inspection.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WeighingService implements WeighingServiceInterface {
+
+
+    @Override
+    public float calculateWeightOfOneProduct(float totalWeight, int numberOfProducts, int quantityOfProductsInPackaging,
+                                             float packagingWeight, float paletteWeight) {
+        int numberOfPackages = numberOfProducts / quantityOfProductsInPackaging;
+        float totalWeightOfPackagingAndPalette = (numberOfPackages * packagingWeight) + paletteWeight;
+        float productWeight = totalWeight - totalWeightOfPackagingAndPalette;
+        return productWeight / numberOfProducts;
+    }
+
+    @Override
+    public float calculateExpectedWeight(int numberOfProducts, float weightOfOneProduct,
+                                         float packagingWeight, float paletteWeight) {
+        float weightOfProducts = numberOfProducts * weightOfOneProduct;
+        return weightOfProducts + packagingWeight + paletteWeight;
+
+    }
+}
