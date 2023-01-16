906afafbb38e3189a77269a5f43d120e4fd20dad
dominik <aplikas7@gmail.com>
2023-01-16 22:52:10 +0100

    backend email export files

--- backend/src/main/java/com/example/weight_inspection/services/EmailSenderService.java
+++ backend/src/main/java/com/example/weight_inspection/services/EmailSenderService.java
@@ -1,24 +1,34 @@
 package com.example.weight_inspection.services;
 
+import com.example.weight_inspection.models.Weighing;
+import com.example.weight_inspection.repositories.WeighingRepository;
+import com.opencsv.CSVWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-import java.io.File;
+import javax.mail.util.ByteArrayDataSource;
+import javax.sql.DataSource;
+import java.io.*;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Service
 public class EmailSenderService {
     @Autowired
     private JavaMailSender mailSender;
-    @Value("${spring.mail.username}") private  String sender;
+
+    @Autowired
+    private WeighingRepository weighingRepository;
+
+    @Value("${spring.mail.username}") private String sender;
 
     @Async
     public void sendNotificationEmail(String[] recipients, String subject, String body) {
@@ -28,8 +38,42 @@ public class EmailSenderService {
         message.setSubject(subject);
         message.setText(body);
         mailSender.send(message);
-
     }
+
+    ByteArrayDataSource createExportCSVFile(Weighing[] weighings) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        OutputStreamWriter streamWriter = new OutputStreamWriter(outputStream);
+        CSVWriter writer = new CSVWriter(streamWriter);
+        writer.writeNext(new String[] {
+                "Id",
+                "IDP",
+                "Úspešné váženie",
+                "Vypočítaná váha",
+                "Nameraná váha",
+                "Dátum váženia",
+                "Počet produktov",
+                "Referencia",
+                "Paleta",
+                "Obal"
+        });
+        for(Weighing weighing: weighings) {
+            writer.writeNext(new String[] {
+                    Long.toString(weighing.getId()),
+                    weighing.getIDP(),
+                    Boolean.toString(weighing.isCorrect()),
+                    Float.toString(weighing.getCalculatedWeight()),
+                    Float.toString(weighing.getWeight()),
+                    weighing.getWeighedOn().toString(),
+                    Integer.toString(weighing.getQuantity()),
+                    weighing.getProduct().getReference(),
+                    weighing.getPalette().getName(),
+                    weighing.getPackaging().getName()
+            });
+        }
+        writer.close();
+        return new ByteArrayDataSource(outputStream.toByteArray(), "text");
+    }
+
     @Async
     public void  sendEmailWithExports(String[] recipients, String subject, String body,
                                        String pathToAttachment) {
@@ -41,10 +85,15 @@ public class EmailSenderService {
             helper.setSubject(subject);
             helper.setText(body);
             FileSystemResource file = new FileSystemResource(new File(pathToAttachment));
-            helper.addAttachment("Exporty", file);
+            Weighing[] weighings = weighingRepository.findNotExportedWeighings();
+            helper.addAttachment("Exporty.csv", createExportCSVFile(weighings));
             mailSender.send(message);
+            Arrays.stream(weighings).forEach(weighing -> {
+                weighing.setExported(true);
+                weighingRepository.save(weighing);
+            });
         }
-        catch (MessagingException e) {
+        catch (MessagingException | IOException e) {
             e.printStackTrace();
         }
 
