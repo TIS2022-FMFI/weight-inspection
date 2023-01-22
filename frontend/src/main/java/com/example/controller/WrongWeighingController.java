90b5e996dc77e24ee5de5f721bae8f20209322fc
flakeua <deemahh2@gmail.com>
2023-01-22 20:43:39 +0100

    Update WrongWeighingController.java

--- frontend/src/main/java/com/example/controller/WrongWeighingController.java
+++ frontend/src/main/java/com/example/controller/WrongWeighingController.java
@@ -2,22 +2,37 @@ package com.example.controller;
 
 import com.example.scene.SceneName;
 import com.example.scene.SceneNavigator;
+import com.example.utils.AdminState;
 import com.example.utils.WorkerState;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class WrongWeighingController extends ScannerController implements Swappable {
 
     @FXML
     public Label packLabel;
     @FXML
+    public ImageView packPhoto;
+    @FXML
     public Label paletteLabel;
+    @FXML
+    public ImageView palettePhoto;
 
     @Override
     public void onLoad(SceneName previousSceneName) {
         packLabel.setText(WorkerState.getWorkerState().getWeighing().getPackaging().getName());
+        Image image = new Image(
+                AdminState.getServer() + WorkerState.getWorkerState().getWeighing().getPackaging().getPicturePath(),
+                true);
+        packPhoto.setImage(image);
         paletteLabel.setText(WorkerState.getWorkerState().getWeighing().getPalette().getName());
+        Image image2 = new Image(
+                AdminState.getServer() + WorkerState.getWorkerState().getWeighing().getPalette().getPicturePath(),
+                true);
+        palettePhoto.setImage(image2);
     }
 
     @Override
